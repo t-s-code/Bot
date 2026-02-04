68432545656f7ae743f855401541301c782c5562
t-s-code <trentstarbine@gmail.com>
2026-02-04 02:10:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ Application entry point.
 
 Responsibilities:
 - Initialize Discord client
-- Load configuration
-- Initialize services
-- Route Discord events to the message processor
+- Wire services and jobs together
+- Schedule high level bot workflows
 """
 
+import asyncio
 import discord
 
 from core.dry_run import DryRun
@@ -20,6 +20,7 @@ from channels.log_channel import LogChannel
 
 from jobs.member_activity_job import MemberActivityJob
 from jobs.channel_pruning_job import ChannelPruningJob
+from jobs.channel_scan_job import ChannelScanJob
 
 
 class Bot(discord.Client):
@@ -29,7 +30,9 @@ class Bot(discord.Client):
 
         self._is_dry_run = is_dry_run
         self._config_channel_id = config_channel_id
-        
+
+        self._processing_lock = asyncio.Lock()
+
         self.dry_run = None
 
         self.config_channel = None
@@ -38,35 +41,31 @@ class Bot(discord.Client):
 
         self.member_activity_job = None
         self.channel_pruning_job = None
-
+        self.channel_scan_job= None
+        
         self.message_processor = None
 
     def _build_intents(self) -> discord.Intents:
-        """
-        Define all Discord intents required by the bot.
-        """
         intents = discord.Intents.default()
         intents.message_content = False
         intents.members = False
         return intents
 
     async def setup_hook(self):
-        """
-        Initialize services and jobs after Discord connection begins.
-        """
-        
-        self.dry_run = DryRun(
-            is_dry_run=self._is_dry_run
-        )
-        
+        self.dry_run = DryRun(is_dry_run=self._is_dry_run)
+
         self.config_channel = ConfigChannel(
             discord_client=self,
             config_channel_id=self._config_channel_id,
         )
 
-        # TODO: everything below this line will need to get reloaded every time the config is updated
-        config = self.config_channel.get_config()
-      
+        config = await self.config_channel.get_config()
+
+        self._build_config_dependent_services(config)
+
+        asyncio.create_task(self.run_periodic_jobs())
+
+    def _build_config_dependent_services(self, config):
         self.database_channel = DatabaseChannel(
             discord_client=self,
             config=config,
@@ -97,41 +96,52 @@ class Bot(discord.Client):
             member_activity_job=self.member_activity_job,
             database_channel=self.database_channel,
         )
-
+        
+        self.channel_scan_job = ChannelScanJob(
+            discord_client=self,
+            processing_lock=self._processing_lock,
+            message_processor=self.message_processor,
+            database_channel=self.database_channel,
+        )
+    
+    
     async def on_ready(self):
-        """
-        Called when the bot successfully connects.
-        """
         print(f"Logged in as {self.user}")
 
     async def on_message(self, message):
-        """
-        Delegate message handling to the message processor.
-        """
-        await self.message_processor.process(message)
+        if message.author.bot:
+            return
+
+        await self.channel_scan_job.schedule_scan(
+            channel_id=message.channel.id
+        )
 
     async def run_periodic_jobs(self):
         """
         Background periodic jobs.
-
-        Intended to run:
-        - Inactivity sweeps
-        - Channel pruning
         """
-        pass
+        while True:
+            await asyncio.sleep(300)
+
+            async with self._processing_lock:
+                await self.member_activity_job.run_inactivity_sweep()
+                await self.channel_pruning_job.run_pruning_sweep()
+
+    async def request_hot_reload(self):
+        """
+        Reload config safely when bot is idle.
+        """
+        async with self._processing_lock:
+            config = await self.config_channel.get_config()
+            self._build_config_dependent_services(config)
 
 
 def main():
-    """
-    Application bootstrap.
-    """
-    # TODO read both of these from command line args
     is_dry_run = None
     config_channel_id = None
-    
+
     bot = Bot(is_dry_run, config_channel_id)
 
-    # Token loading intentionally omitted (config/env later)
     # bot.run("TOKEN")
     pass
 

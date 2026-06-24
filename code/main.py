d457e1877c3a057cb3099bdbc3d3db0fd4780556
t-s-code <trentstarbine@gmail.com>
2026-06-24 14:42:08 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,31 +1,23 @@
 # main.py
 
-"""
-Application entry point.
-
-Responsibilities:
-- Initialize Discord client
-- Wire services and jobs together
-- Schedule high level bot workflows
-"""
-
-import asyncio
-import discord
-
-from core.dry_run import DryRun
-from core.message_processor import MessageProcessor
-
-from channels.database_channel import DatabaseChannel
-from channels.log_channel import LogChannel
-
-from jobs.channel_scanning_job import ChannelScanningJob
-
+from our_bot import OurBot
+from models.config import *
 
 def main():
-    is_dry_run = None
+    is_dry_run = True
     token = None
 
-    bot = Bot(is_dry_run)
+    config = BotConfig(
+        channel_pruning_configs = [
+            ChannelPruningConfig(
+                channel_name="",
+                channel_id=1,
+                days_until_delete_messages_from_channel=3
+            )
+        ]
+    )
+
+    bot = Bot(is_dry_run, config)
     bot.run(token)
 
 

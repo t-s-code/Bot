1b975d24ef502a570fe56a0f126f326704c6ac28
t-s-code <trentstarbine@gmail.com>
2026-06-24 13:14:00 -0400

    Update config.py

--- code/models/config.py
+++ code/models/config.py
@@ -7,7 +7,16 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class BotConfig:
     channel_pruning_configs: list[ChannelPruningConfig]
+    
+    def validate(self):
+        seen_ids = set()
+        
+        for c in self.channel_pruning_configs:
+            seen_ids.add(c.channel_id)
+            c.validate()
 
+        if len(seen_ids) != len(self.channel_pruning_configs):
+            raise ValueError("Every channel_id in channel_pruning_configs must be unique")
 
 @dataclass(frozen=True)
 class ChannelPruningConfig:
@@ -15,6 +24,10 @@ class ChannelPruningConfig:
     channel_id: int
     days_until_delete_messages_from_channel: int
 
+    def validate(self):
+        if self.days_until_delete_messages_from_channel < 1:
+            raise ValueError("days_until_delete_messages_from_channel must be > 0")
+            
 bot_config = BotConfig(
     channel_pruning_configs = [
         ChannelPruningConfig(

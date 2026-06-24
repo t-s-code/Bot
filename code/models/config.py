8b690e426e006eef5dffa4e4d568ef9e727cd322
t-s-code <trentstarbine@gmail.com>
2026-06-24 13:05:06 -0400

    Update config.py

--- code/models/config.py
+++ code/models/config.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class Config:
+class BotConfig:
     channel_pruning_configs: list[ChannelPruningConfig]
 
 
@@ -13,4 +13,14 @@ class Config:
 class ChannelPruningConfig:
     channel_name: str
     channel_id: int
-    days_until_delete_messages: int
+    days_until_delete_messages_from_channel: int
+
+bot_config = BotConfig(
+    channel_pruning_configs = [
+        ChannelPruningConfig(
+            channel_name="",
+            channel_id=1,
+            days_until_delete_messages_from_channel=3
+        )
+    ]
+)

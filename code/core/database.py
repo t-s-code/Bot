c5ce084784d14eb1036917a9f1d6329b57596cd6
Trent <trentstarbine@gmail.com>
2026-06-25 19:09:02 +0000

    stub database

---
+++ code/core/database.py
@@ -0,0 +1,32 @@
+# core/database.py
+
+import asyncio
+from .in_memory_database import InMemoryDatabase
+
+class Database:
+    def __init__(self, discord_client):
+        self._discord_client = discord_client
+        self._lock = asyncio.Lock()
+        self._in_memory_database = None
+
+    async def load(self):
+        async with self._lock:
+            # TODO: messages_from_db = load all messages from the #bot-database channel
+            self._in_memory_database = InMemoryDatabase(messages_from_db)
+
+    async def get_channel_scanning_cursor(self, channel_id):
+        async with self._lock:
+            return self._in_memory_database.get_channel_scanning_cursor(channel_id)
+
+    async def get_member_activity(self, user_id):
+        async with self._lock:
+            return self._in_memory_database.get_member_activity(user_id)
+
+    def queue_update(self, record):
+        # TODO
+        pass
+
+    async def commit_updates(self):
+        async with self._lock:
+            # TODO: commit queued updates to #bot-database channel and update in memory db
+            pass
---
+++ code/core/in_memory_database.py
@@ -0,0 +1,14 @@
+# core/in_memory_database.py
+
+class InMemoryDatabase:
+    def __init__(self, messages_from_database_channel):
+        # TODO
+        pass
+
+    def get_channel_scanning_cursor(self, channel_id):
+        # TODO
+        pass
+
+    def get_member_activity(self, user_id):
+        # TODO
+        pass
